5d80ff8f438f003dccb5a6c01590e87daaa2ad0d
KOP <KOP@KOP-PC.clearos.lan>
2015-09-05 12:03:55 +0700

    Merge origin/master
    Conflicts:
    	composer.json
    	composer.lock
    	config/web.php

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=192.168.1.251;dbname=hos',
+    'username' => 'chklock',
+    'password' => 'chklock',
+    'charset' => 'utf8',
+];
---
+++ views/site/index.php
@@ -0,0 +1,20 @@
+<?php
+
+Yii::$app->db->open();
+/* @var $this yii\web\View */
+
+$this->title = 'รพ.เชียงคาน';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>ติดตั้ง Yii สำเร็จแล้วครับ</h1>
+
+        <p class="lead">วันที่ 5 กันยายน 2558</p>
+
+        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    </div>
+    
+</div>
+
+    
